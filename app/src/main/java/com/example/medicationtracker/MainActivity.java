fd72815727c1cd0160f8f7037e61bfa69ba3dfad
DESKTOP-D10N1VT\Jia Hao <dontaskmestuff@gmail.com>
2017-05-31 01:10:52 +0800

    PrescriptionListActivity added

--- app/src/main/java/com/example/medicationtracker/MainActivity.java
+++ app/src/main/java/com/example/medicationtracker/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.medicationtracker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /*
+    starts the PrescriptionListActivity
+    used in button_medications onClick
+     */
+    public void onClickMedications(View v) {
+        Intent intent = new Intent(this, PrescriptionListActivity.class);
+        startActivity(intent);
     }
 }
