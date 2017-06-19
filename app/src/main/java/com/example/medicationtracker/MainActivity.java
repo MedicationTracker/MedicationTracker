894bfc5d1f510c235712644e2045b9032d6a4ab2
Ryan Tan Wei Keat <ryantan2103@gmail.com>
2017-06-19 22:29:38 +0800

    Delete Jia Hao Comment

--- app/src/main/java/com/example/medicationtracker/MainActivity.java
+++ app/src/main/java/com/example/medicationtracker/MainActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-    // test change by Jia hao today
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
