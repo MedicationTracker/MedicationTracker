0c01171faf215b6cc204a95f3c45185e89d7c729
Ryan Tan Wei Keat <ryantan2103@gmail.com>
2017-06-19 23:01:58 +0800

    Add alarm

--- app/src/main/java/com/example/medicationtracker/MainActivity.java
+++ app/src/main/java/com/example/medicationtracker/MainActivity.java
@@ -1,25 +1,42 @@
 package com.example.medicationtracker;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.app.AlarmManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
-    // test commit again haha
+
+    // instantiate variables
+    AlarmManager alarm_manager;
+    TimePicker time_picker;
+    Calendar calendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    /*
-    starts the PrescriptionListActivity
-    used in button_medications onClick
-     */
-    public void onClickMedications(View v) {
-        Intent intent = new Intent(this, PrescriptionListActivity.class);
-        startActivity(intent);
+        // assign variables
+        alarm_manager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        time_picker = (TimePicker) findViewById(R.id.timePicker);
+        calendar = Calendar.getInstance();
+
+        // initialise buttons
+        Button set_time = (Button) findViewById(R.id.set_time);
+        Button unset_time = (Button) findViewById(R.id.unset_time);
+
+        // onclicklistener to set alarm
+        set_time.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Toast.makeText(this, "Hi", Toast.LENGTH_SHORT);
+            }
+            });
+
     }
 }
